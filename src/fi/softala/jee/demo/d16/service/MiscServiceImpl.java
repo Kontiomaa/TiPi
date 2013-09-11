2ca51e36a9b4165aaa3cf13bd06bab6597893efe
Lauri Soivi <soivi.lauri@gmail.com>
2013-09-11 12:12:14 +0300

    Added sign in example

---
+++ src/fi/softala/jee/demo/d16/service/MiscServiceImpl.java
@@ -0,0 +1,23 @@
+package fi.softala.jee.demo.d16.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MiscServiceImpl implements MiscService {
+	
+	@Override
+	public void palveluAdmineille() {
+		System.out.println("misc admineille");
+	}
+	
+	@Override
+	public void palveluSisaankirjautuneille() {
+		System.out.println("misc sisaankirjautuneille");
+	}
+	
+	@Override
+	public void palveluKaikille() {
+		System.out.println("misc kaikille");
+	}
+
+}
