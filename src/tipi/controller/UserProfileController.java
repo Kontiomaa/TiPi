c1403303fdbaf606a1d18fed8764686d990e4f09
Samuel Kontiomaa <samuel.kontiomaa@gmail.com>
2013-10-21 15:44:05 +0300

    Cleaned last commit (removed extra prints to console and imports)

--- src/tipi/controller/UserProfileController.java
+++ src/tipi/controller/UserProfileController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import tipi.bean.UserProfile;
 import tipi.bean.UserProfileImpl;
 import tipi.service.UserProfileService;
 
@@ -22,9 +21,7 @@ import tipi.service.UserProfileService;
 public class UserProfileController {
 
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	public String profile(Model model, @ModelAttribute(value = "userProfile") UserProfileImpl juuseri) {
-		//tuhoa t‰m‰ sysoon asti!
-		System.out.println("salis ennen vaihtotoimenpiteit‰ "+juuseri.getPassword());
+	public String profile(Model model) {
 		return "/user/profile";
 	}
 	
@@ -41,33 +38,19 @@ public class UserProfileController {
 	
 	@RequestMapping(value = "/changePassword", method = RequestMethod.POST)
 	public String changePassword(Model model, HttpServletRequest req, @ModelAttribute(value = "userProfile") UserProfileImpl userProfile) {
-		System.out.println("Controller: "+req.getParameter("oldPassword"));
-		System.out.println(req.getParameter("newPassword"));
-		
-		
 		
 		StandardPasswordEncoder spe = new StandardPasswordEncoder();
-		String newPassword=spe.encode(req.getParameter("newPassword")); //Password is now encrypted for safety
-		System.out.println("New password encrypted: "+newPassword);
-		
-		//TODO check how to see if the old password matches the written one... Can I use the same field as when users are logging in?
-
-		//if correct send the new password to dao in order to get it into the database
-		
-		System.out.println(req.getParameter("oldPassword"));
-		System.out.println(userProfile.getPassword());
+		String newPassword=spe.encode(req.getParameter("newPassword"));
 		
 		boolean correctPassword = spe.matches(req.getParameter("oldPassword"), userProfile.getPassword());
-		System.out.println(correctPassword);
+		
 		if(correctPassword){
 			String email = userProfile.getEmail();
-			System.out.println("Email: " +email);
 			userProfileService.sendNewPasswordToDao(email, newPassword);
-			System.out.println("Great Success! No? Little success?! Any success??");
 		}
 		else
 		{
-			System.out.println("V‰‰rin meni!");
+			System.out.println("Wrong password!");
 		}
 		
 		return "/user/profile";
